89cfd880f6a0f91583968650f888efb3be1a1806
Tanmay Dobariya <dobariyatanmay@gmail.com>
2023-09-21 14:28:03 +0530

    Iphone

---
+++ index.js
@@ -0,0 +1,18 @@
+const circle = document.getElementById('circle');
+const upBtn = document.getElementById('UpArrowBtn');
+const downBtn = document.getElementById('DownArrowBtn');
+
+let rotationValue = circle.style.transform;
+let calculatedRotationValue;
+
+upBtn.onclick = () => {
+    calculatedRotationValue = rotationValue + 'rotate(-90deg)';
+    circle.style.transform = calculatedRotationValue;
+    rotationValue = calculatedRotationValue;
+}
+
+downBtn.onclick = () => {
+    calculatedRotationValue = rotationValue + 'rotate(90deg)';
+    circle.style.transform = calculatedRotationValue;
+    rotationValue = calculatedRotationValue;
+}
